a3da5975ee0f6d7201d2028fb1c053c20998e3d4
user <ujjalroy7862@gmail.com>
2022-11-05 14:46:49 +0600

    Done this assignment accoding to the instructions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Counter = () => {
+  const [count, setCounter] = useState(0);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : { count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn"
+          onClick={()=>setCounter(count+1)}disabled={count === 5 ? true : false}>+</button>
+          <button className="btn card__btn"  onClick={()=>setCounter(count+1)}disabled={count === -5 ? true : false}> -</button>
+          <button className="btn card__btn"  onClick={()=>setCounter(count = 0)}>0</button>
         </div>
       </div>
     </div>
